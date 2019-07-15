d20e414f8e7bbfc62980d26235facd990a24f1b3
ferdoms <marinhosilva.fernando@gmail.com>
2019-07-15 21:42:35 +0100

    card component added

---
+++ src/robojs.js
@@ -0,0 +1,78 @@
+export const robots = [
+    [{
+        "id": 1,
+        "name": "Nonie Heel",
+        "username": "nheel0",
+        "email": "nheel0@jimdo.com"
+      }, {
+        "id": 2,
+        "name": "Holly-anne Dorton",
+        "username": "hdorton1",
+        "email": "hdorton1@kickstarter.com"
+      }, {
+        "id": 3,
+        "name": "Dari Camelli",
+        "username": "dcamelli2",
+        "email": "dcamelli2@deviantart.com"
+      }, {
+        "id": 4,
+        "name": "Cyrillus Laste",
+        "username": "claste3",
+        "email": "claste3@seattletimes.com"
+      }, {
+        "id": 5,
+        "name": "Joice Godby",
+        "username": "jgodby4",
+        "email": "jgodby4@reuters.com"
+      }, {
+        "id": 6,
+        "name": "Fey Rosenthal",
+        "username": "frosenthal5",
+        "email": "frosenthal5@tinypic.com"
+      }, {
+        "id": 7,
+        "name": "Wade Aynscombe",
+        "username": "waynscombe6",
+        "email": "waynscombe6@cargocollective.com"
+      }, {
+        "id": 8,
+        "name": "Lonnie Kitchinghan",
+        "username": "lkitchinghan7",
+        "email": "lkitchinghan7@scribd.com"
+      }, {
+        "id": 9,
+        "name": "Gery Goldfinch",
+        "username": "ggoldfinch8",
+        "email": "ggoldfinch8@eventbrite.com"
+      }, {
+        "id": 10,
+        "name": "Sharla Wetherburn",
+        "username": "swetherburn9",
+        "email": "swetherburn9@techcrunch.com"
+      }, {
+        "id": 11,
+        "name": "Vitoria Addyman",
+        "username": "vaddymana",
+        "email": "vaddymana@mail.ru"
+      }, {
+        "id": 12,
+        "name": "Tomaso Oxer",
+        "username": "toxerb",
+        "email": "toxerb@dion.ne.jp"
+      }, {
+        "id": 13,
+        "name": "Suki Masters",
+        "username": "smastersc",
+        "email": "smastersc@etsy.com"
+      }, {
+        "id": 14,
+        "name": "Massimiliano Kemster",
+        "username": "mkemsterd",
+        "email": "mkemsterd@bing.com"
+      }, {
+        "id": 15,
+        "name": "Horacio Anlay",
+        "username": "hanlaye",
+        "email": "hanlaye@instagram.com"
+      }]
+]
