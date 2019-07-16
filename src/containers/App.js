6be4393ddc9fcfd9488c138328e10ad04d850730
ferdoms <marinhosilva.fernando@gmail.com>
2019-07-16 08:59:22 +0100

    directory restructured

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,20 +1,24 @@
 import React, { Component } from "react";
-import CardList from "./CardList";
-import { robots } from "./robots";
-import SearchBox from "./SearchBox";
+import CardList from "../components/CardList";
+import SearchBox from "../components/SearchBox";
 import "./App.css"
 
 export class App extends Component {
   constructor() {
     super();
     this.state = {
-      robots: robots,
+      robots: [],
       searchField: ""
     };
   }
 
+  componentDidMount(){
+      fetch('https://jsonplaceholder.typicode.com/users')
+       .then(response => response.json())
+       .then(users => this.setState({ robots: users}));
+  }
   onSearchChange = (event) => {
-    this.setState({searchField: event.target.value})
+    this.setState({searchField: event.target.value});
   }
 
   render() {
